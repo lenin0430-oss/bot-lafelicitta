dd6d20301a8b0589407f4d547d842b939604d327
lenin0430-oss <lenin0430@gmail.com>
2026-04-10 18:09:52 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,9 +66,10 @@ const client = new Client({
   authStrategy: new LocalAuth({
     dataPath: process.env.SESSION_PATH || "./.wwebjs_auth",
   }),
-  puppeteer: {
+ puppeteer: {
     headless: true,
     executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
+    protocolTimeout: 120000,
     args: ["--no-sandbox","--disable-setuid-sandbox","--disable-dev-shm-usage","--disable-accelerated-2d-canvas","--no-first-run","--no-zygote","--disable-gpu"],
   },
 });
